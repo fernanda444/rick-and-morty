b15f5f92d26e980f2a26c0c12b3cf6668499c654
fernanda444 <69698408+fernanda444@users.noreply.github.com>
2025-04-25 20:11:29 +0000

    personajes

--- src/componentes/personajes/index.jsx
+++ src/componentes/personajes/index.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './style.css'
 
 function Personajes () {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      const res = await fetch(`https://rickandmortyapi.com/api/character`);
+      const json = await res.json();
+      setData(json.results);
+    };
 
+    obtenerDatos();
+  }, []);
   return (
     <>
-    <h1>Personajes</h1>
+    {console.log(data)}
+    {data.map((unpersonaje, index) => (
+        <div className='c-personajes'
+        key={index}>
+          <p>{unpersonaje.name}</p>
+          <p>{unpersonaje.id}</p>
+          <p>{unpersonaje.status}</p>
+          <p>{unpersonaje.species}</p>
+          <p>{unpersonaje.type}</p>
+          <p>{unpersonaje.gender}</p>
+          <img src={unpersonaje.image} alt="" />
+
+        </div>
+      ))}
     </>
+    
   )
 }
 
